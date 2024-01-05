b2c489061ae2a3f71f4c54fde35ac2575ef301c2
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-05 19:03:18 +0600

    3.5_displaying all receipt info_E

--- resources/js/components/invoices/index.vue
+++ resources/js/components/invoices/index.vue
@@ -77,7 +77,8 @@ const getInvoices = async() => {
                     <a href="#" class="table--items--transactionId">#{{item.id}}</a>
                     <p>{{ item.date}}</p>
                     <p>{{item.number}}</p>
-                    <p> {{item.customer_id}}</p>
+                    <p v-if="item.customer"> {{item.customer.firstname}}</p>
+                    <p v-else></p>
                     <p> {{item.due_date}}</p>
                     <p> ${{ item.total }} </p>
                 </div>
